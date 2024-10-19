e6425fc4295ee81e8e598241b57abca98bed8317
zolochivska0990 bc2024-4 <anastasiazolocivskp@gmail.com>
2024-10-19 21:56:11 +0300

    Implement file read, write and delete functionality

--- server.js
+++ server.js
@@ -1,21 +1,53 @@
-const http = require('http');
-const { Command } = require('commander');
-const program = new Command();
+const fs = require('fs').promises;
+const path = require('path');
 
-program
-  .requiredOption('-h, --host <host>', 'address of the server')
-  .requiredOption('-p, --port <port>', 'port of the server')
-  .requiredOption('-c, --cache <path>', 'path to cache directory');
+const cacheDir = options.cache;
 
-program.parse(process.argv);
+const handleRequest = async (req, res) => {
+    const code = req.url.substring(1); // отримати код з URL
 
-const options = program.opts();
+    switch (req.method) {
+        case 'GET':
+            try {
+                const filePath = path.join(cacheDir, `${code}.jpg`);
+                const data = await fs.readFile(filePath);
+                res.writeHead(200, {'Content-Type': 'image/jpeg'});
+                res.end(data);
+            } catch (error) {
+                res.writeHead(404, {'Content-Type': 'text/plain'});
+                res.end('Not Found');
+            }
+            break;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Server is running');
-});
+        case 'PUT':
+            const chunks = [];
+            req.on('data', chunk => chunks.push(chunk));
+            req.on('end', async () => {
+                const buffer = Buffer.concat(chunks);
+                const filePath = path.join(cacheDir, `${code}.jpg`);
+                await fs.writeFile(filePath, buffer);
+                res.writeHead(201, {'Content-Type': 'text/plain'});
+                res.end('Created');
+            });
+            break;
 
-server.listen(options.port, options.host, () => {
-    console.log(`Server running at http://${options.host}:${options.port}/`);
-});
+        case 'DELETE':
+            try {
+                const filePath = path.join(cacheDir, `${code}.jpg`);
+                await fs.unlink(filePath);
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.end('Deleted');
+            } catch (error) {
+                res.writeHead(404, {'Content-Type': 'text/plain'});
+                res.end('Not Found');
+            }
+            break;
+
+        default:
+            res.writeHead(405, {'Content-Type': 'text/plain'});
+            res.end('Method not allowed');
+            break;
+    }
+};
+
+const server = http.createServer(handleRequest);
